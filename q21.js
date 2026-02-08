6cc127397f5b68d5d5c5e87170690c7dd3bfd6dc
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-08 19:22:11 +0530

    Create q21.js

---
+++ q21.js
@@ -0,0 +1,26 @@
+db.createCollection("vendors", {              
+    validator:{
+        $jsonSchema: {
+            bsonType:"object",
+            required:["name","age"],
+            properties:{
+                name:{bsonType:"string"},
+                age:{bsonType:"int",minimum:18}
+            }
+        }
+    }
+})
+
+db.vendors.insertOne({
+    name:"John",
+    age:21
+})
+
+db.vendors.insertOne({
+    age:16
+})
+
+db.vendors.insertOne({
+    name:12306232,
+    age:"asdfghjkl"
+})
